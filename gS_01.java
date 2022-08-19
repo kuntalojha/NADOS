b7b36dc59fac4e1075091e795768f988b1f1bef0
Kuntal Ojha <kuntalojha2021@gmail.com>
2022-08-19 09:30:22 +0000

    Delate

--- gS_01.java
+++ gS_01.java
@@ -1,4 +1,4 @@
-public class gettingStarted_01 {
+public class gS_01 {
 	public static void main(String[] args) {
 		System.out.println("*");
 		System.out.println("**");
--- gS_01_1.java
+++ gS_01_1.java
@@ -1,4 +1,4 @@
-public class gettingStarted_01_ {
+public class gS_01_1 {
 	public static void main(String[] args) {
 		System.out.println("*\n**\n***\n****\n*****\n******");
 	}
