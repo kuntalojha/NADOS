a97da3196d7264afe91a29ea1150d9e304a48648
Kuntal Ojha <kuntalojha2021@gmail.com>
2022-08-23 18:12:06 +0000

    23-08-2022

---
+++ gettingStarted_8_Loop.java
@@ -0,0 +1,17 @@
+public class gettingStarted_8_Loop {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=9)
+        {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("DONE While Loop!");
+
+        for( i=1; i<=9;i++)
+        {
+            System.out.println(i);
+        }
+        System.out.println("DONE for loop!");
+    }
+}
